2c42ae8372d7bb5b75310392fde72f5a7f1e6f79
dongsun1 <le12352@naver.com>
2022-04-01 19:22:05 +0900

    90% 구현

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
-class Sensor {}
+class Sensor {
+    constructor() {
+        this.powerStatus = 'off';
+        this.reportingInterval = 10000;
+    }
 
-class IotServer {}
+    turn(method) {
+        if (method === 'off') {
+            this.status = 'idle';
+            this.powerStatus = method;
+            return;
+        }
+        if (this.powerStatus === 'on') {
+            throw new Error();
+        }
+        this.status = 'idle';
+        this.powerStatus = method;
+
+        setTimeout(() => {
+            this.status = 'sensingDistance';
+        }, this.reportingInterval);
+
+        setTimeout(() => {
+            this.status = 'reportingData';
+        }, this.reportingInterval + 500);
+
+        setTimeout(() => {
+            this.status = 'idle';
+        }, this.reportingInterval + 500 + 1000);
+    }
+}
+
+class IotServer {
+    start(sensor) {}
+
+    publish(props) {
+        reportingInterval = props.payload;
+    }
+}
 
 module.exports = {
     Sensor,
